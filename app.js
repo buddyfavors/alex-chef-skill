1b5e0d34aee0341dfac21e9dbca31200d029b67e
Buddy Favors Jr <buddyfavors@gmail.com>
2017-02-18 22:48:03 -0500

    Migrated to the local Alexa server. Chef will no longer suggest the same thing in a session.

--- app.js
+++ app.js
@@ -1,101 +1,104 @@
 'use strict';
 
-const Alexa = require('alexa-sdk');
+module.change_code = 1;
 
-const APP_ID = 'amzn1.ask.skill.9c376d24-a2aa-471a-9b65-cec03b6bb99b';
+const alexa = require('alexa-app');
 
-const languageStrings = {
-    'en-US': {
-        translation: {
-            DINNERS: [
-                'Little Mexico',
-                'Barefoot Burger',
-                'Creekside Lodge',
-                'Brother\'s Pizza',
-                'Yamato',
-                'Beijing Chinese',
-                'Allen\'s Country Kitchen',
-                'Wings Etc',
-                'The Forum',
-                'Blondie\'s BBQ',
-                'China Inn',
-                'Jimmy John\'s',
-                'Culver\'s',
-                'Mama Fazio\'s Pizza',
-                'Steak \'n Shake',
-                'Applebee\'s',
-                'Marco\'s Pizza',
-                'Pizza Hut',
-                'Dominos',
-                'Cracker Barrel',
-                'Buffalo Wild Wings',
-                'Arby\'s',
-                'Taco Bell',
-                'Burger King',
-                'Subway',
-                'Wendy\'s',
-                'Dairy Queen',
-                'Long John Silver',
-                'McDonald\'s',
-                'KFC'
-            ],
-            SKILL_NAME: 'Chef',
-            GET_DINNER_MESSAGE: 'What about ',
-            HELP_MESSAGE: 'You can say what is for dinner, or, you can say exit, What can I help you with?',
-            HELP_REPROMPT: 'What can I help you with?',
-            STOP_MESSAGE: 'Enjoy your meal!',
-        }
+const app = new alexa.app('chef');
+
+const model = {
+    DINNERS: [
+        'Little Mexico',
+        'Barefoot Burger',
+        'Creekside Lodge',
+        'Brother\'s Pizza',
+        'Yamato',
+        'Beijing Chinese',
+        'Allen\'s Country Kitchen',
+        'Wings Etc',
+        'The Forum',
+        'Blondie\'s BBQ',
+        'China Inn',
+        'Jimmy John\'s',
+        'Culver\'s',
+        'Mama Fazio\'s Pizza',
+        'Steak \'n Shake',
+        'Applebee\'s',
+        'Marco\'s Pizza',
+        'Pizza Hut',
+        'Dominos',
+        'Cracker Barrel',
+        'Buffalo Wild Wings',
+        'Arby\'s',
+        'Taco Bell',
+        'Burger King',
+        'Subway',
+        'Wendy\'s',
+        'Dairy Queen',
+        'Long John Silver',
+        'McDonald\'s',
+        'KFC'
+    ],
+    SKILL_NAME: 'Chef',
+    GET_DINNER_MESSAGE: 'What about ',
+    HELP_MESSAGE: 'You can say what is for dinner, or, you can say exit, What can I help you with?',
+    HELP_REPROMPT: 'What can I help you with?',
+    STOP_MESSAGE: 'Enjoy your meal!',
+};
+
+function getDinnerOption(suggestions) {
+    const dinnerOptions = model.DINNERS;
+    const optionIndex = Math.floor(Math.random() * dinnerOptions.length);
+    const dinnerOption =  dinnerOptions[optionIndex];
+    dinnerOptions.splice(optionIndex, 1);
+    return dinnerOption;
+}
+
+app.launch(function(req, res) {
+  res.session('suggestions', []);
+
+  const prompt = model.HELP_MESSAGE;
+  res.say(prompt).reprompt(prompt).shouldEndSession(false);
+});
+
+app.intent('GetDinnerIntent', {
+  "slots": {
+    "YES_ACTION": "LITERAL",
+    "NO_ACTION": "LITERAL"
+  },
+  "utterances": [
+    "{yes|yeah|yep|sure|ok|YES_ACTION}",
+    "{no|nope|NO_ACTION}"
+  ]
+},
+  function(req, res) {
+    const yesAction = req.slot('YES_ACTION');
+
+    if (yesAction) {
+      res.say(model.STOP_MESSAGE);
+      return;
     }
-};
-const handlers = {
-    'LaunchRequest': function () {
-        this.emit('AMAZON.HelpIntent');
-    },
-    'NewSession': function () {
-        this.emit('GetDinnerIntent');
-    },
-    'GetDinnerIntent': function () {
-        if (this.attributes['dinnerOptions'] === undefined) {
-            this.attributes['dinnerOptions'] = [];
-        }
-        const dinnerOptions = this.t('DINNERS');
-        const optionIndex = Math.floor(Math.random() * dinnerOptions.length);
-        const dinnerOption =  dinnerOptions[optionIndex];
-        this.attributes['dinnerOptions'].push(dinnerOption);
-        console.log(this.attributes['dinnerOptions']);
-        console.log(this.attributes['dinnerOptions'].length);
-        const speechOutput = this.t('GET_DINNER_MESSAGE') + dinnerOption;
-        this.emit(':ask', speechOutput);
-    },
-    'AMAZON.YesIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
-    'AMAZON.NoIntent': function () {
-        const dinnerOptions = this.t('DINNERS');
-        const optionIndex = Math.floor(Math.random() * dinnerOptions.length);
-        const speechOutput =  dinnerOptions[optionIndex];
-        this.emit(':ask', speechOutput);
-    },
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = this.t('HELP_MESSAGE');
-        const reprompt = this.t('HELP_MESSAGE');
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
-    'SessionEndedRequest': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    }
-};
 
-exports.handler = (event, context) => {
-    const alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
+    const noAction = req.slot('NO_ACTION');
+    const suggestions = req.session('suggestions');
+
+    if (!noAction && suggestions.length >= 0) {
+      res.reprompt("Sorry, I didn't understand. Try again.").shouldEndSession(false);
+      return;
+    }
+    var dinnerOption = getDinnerOption(suggestions);
+
+    if (dinnerOption === null) {
+      res.say("Sorry, I'm out of suggestions.");
+      return;
+    }
+
+    suggestions.push(dinnerOption);
+    res.session('suggestions', suggestions);
+
+    const prompt = (suggestions.length == 0 ? model.GET_DINNER_MESSAGE : '') + dinnerOption;
+    res.say(prompt).shouldEndSession(false);
+  }
+);
+
+module.exports = app;
